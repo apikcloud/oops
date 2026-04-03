4478b290ac36df2b98b76ec28d4ac105b26f5fa1
Aurelien ROY <aro@apik.cloud>
2026-04-03 21:46:44 +0200

    fix(project): fix check command

--- oops/commands/project/check.py
+++ oops/commands/project/check.py
@@ -12,32 +12,38 @@ warnings also cause a non-zero exit.
 """
 
 import click
-from oops.commands.base import command
 
+from oops.commands.base import command
 from oops.commands.project.common import check_project, parse_odoo_version
-from oops.git.core import GitRepository
 from oops.services.docker import check_image, parse_image_tag
+from oops.utils.git import get_local_repo
 from oops.utils.render import render_table
 
 
 @command(name="check", help=__doc__)
-@click.option("--strict", is_flag=True, help="Do not fail on warnings")
+@click.option("--strict", is_flag=True, help="Treat warnings as errors")
 def main(strict: bool):  # noqa: C901
 
-    repo = GitRepository()
+    _, repo_path = get_local_repo()
 
-    warnings, errors = check_project(repo.path, strict=strict)
-    odoo_version = parse_odoo_version(repo.path)
-    image_infos = parse_image_tag(odoo_version)
-    warnings += check_image(image_infos, strict=strict)
+    # Always collect without raising so we can display everything before exiting
+    warns, errors = check_project(repo_path, strict=False)
+
+    try:
+        odoo_version = parse_odoo_version(repo_path)
+        image_info = parse_image_tag(odoo_version)
+        warns += check_image(image_info, strict=False)
+    except ValueError as e:
+        errors.append(str(e))
 
     rows = []
-    if warnings:
-        for row in warnings:
-            rows.append([click.style("Warning", fg="yellow"), click.style(row, fg="yellow")])
+    for msg in warns:
+        rows.append([click.style("Warning", fg="yellow"), click.style(msg, fg="yellow")])
+    for msg in errors:
+        rows.append([click.style("Error", fg="red"), click.style(msg, fg="red")])
 
-    if errors:
-        for row in errors:
-            rows.append([click.style("Error", fg="red"), click.style(row, fg="red")])
+    if rows:
+        click.echo(render_table(rows))
 
-    click.echo(render_table(rows))
+    if errors or (strict and warns):
+        raise click.exceptions.Exit(1)
